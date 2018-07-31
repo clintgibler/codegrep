b1bcfa5478c66cebc58cf8cd17cb50d6012f0104
Syam Puranam <github@syam.in>
2018-07-30 22:05:39 -0700

    style commit

--- utils/clang-dump.c
+++ utils/clang-dump.c
@@ -46,8 +46,7 @@ enum CXChildVisitResult visitor(CXCursor c, CXCursor parent, CXClientData client
   return CXChildVisit_Recurse;
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
  if(argc < 1) {
     return 1;
  }
