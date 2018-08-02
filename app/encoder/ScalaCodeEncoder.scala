a2401c31d510f50977fb982d6b10d8c4d8a65e65
Syam Puranam <github@syam.in>
2018-08-01 20:46:57 -0700

    catch stackoverflow error for scalafmt

--- app/encoder/ScalaCodeEncoder.scala
+++ app/encoder/ScalaCodeEncoder.scala
@@ -8,23 +8,28 @@ object ScalaCodeEncoder {
 
   def parse(content: String): List[TokenModel] = {
     val tokens = new collection.mutable.MutableList[TokenModel]
-    content.parse[Source].get.traverse {
-      case v: Defn.Class =>
-        tokens += TokenModel(v.name.value, v.pos.startLine, v.pos.startColumn, "class")
-      case v: Defn.Object =>
-        tokens += TokenModel(v.name.value, v.pos.startLine, v.pos.startColumn, "object")
-      case v: Defn.Trait =>
-        tokens += TokenModel(v.name.value, v.pos.startLine, v.pos.startColumn, "trait")
-      case v: Defn.Val =>
-        v.tokens
-          .filter(_.productPrefix == "Ident")
-          .foreach((t) => tokens += TokenModel(t.text, t.pos.startLine, t.pos.startColumn, "variable"))
-      case v: Defn.Var =>
-        v.tokens
-          .filter(_.productPrefix == "Ident")
-          .foreach((t) => tokens += TokenModel(t.text, t.pos.startLine, t.pos.startColumn, "variable"))
-      case v: Defn.Def =>
-        tokens += TokenModel(v.name.toString(), v.name.pos.startLine, v.name.pos.startColumn, "function")
+    try {
+      content.parse[Source].get.traverse {
+        case v: Defn.Class =>
+          tokens += TokenModel(v.name.value, v.pos.startLine, v.pos.startColumn, "class")
+        case v: Defn.Object =>
+          tokens += TokenModel(v.name.value, v.pos.startLine, v.pos.startColumn, "object")
+        case v: Defn.Trait =>
+          tokens += TokenModel(v.name.value, v.pos.startLine, v.pos.startColumn, "trait")
+        case v: Defn.Val =>
+          v.tokens
+            .filter(_.productPrefix == "Ident")
+            .foreach((t) => tokens += TokenModel(t.text, t.pos.startLine, t.pos.startColumn, "variable"))
+        case v: Defn.Var =>
+          v.tokens
+            .filter(_.productPrefix == "Ident")
+            .foreach((t) => tokens += TokenModel(t.text, t.pos.startLine, t.pos.startColumn, "variable"))
+        case v: Defn.Def =>
+          tokens += TokenModel(v.name.toString(), v.name.pos.startLine, v.name.pos.startColumn, "function")
+      }
+    } catch {
+      case e: StackOverflowError =>
+        Logger.warn("Encountered stack overflow: %s".format(e.getMessage))
     }
     tokens.toList
   }
